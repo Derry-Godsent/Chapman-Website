222f6982175ba0d08145174b19622a70f806f72c
Derry Kay <derrickbright164@gmail.com>
2026-05-24 23:14:10 +0000

    fix manualChunks build issue

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: "esnext",
-    sourcemap: false,
-  },
-  css: {
-    devSourcemap: false,
-  },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
