f3a97b16ff8d4584a9cbdfe616cb582758cce9ea
Derry Kay <derrickbright164@gmail.com>
2026-05-24 21:51:47 +0000

    fix: replace vite.config.js for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -9,8 +9,7 @@ export default defineConfig({
     terserOptions: {
       compress: { drop_console: true, drop_debugger: true },
     },
-    sourcemap: false, // Reduce build size
-
+    sourcemap: false,
   },
   css: {
     devSourcemap: false,
