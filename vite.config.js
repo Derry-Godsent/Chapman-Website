4741dd9c5fbbe5456d875dd1e5f504f31f1a1590
Derry Kay <derrickbright164@gmail.com>
2026-05-24 22:30:01 +0000

    fix build

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: "esnext",
-    minify: "terser",
-    terserOptions: {
-      compress: { drop_console: true, drop_debugger: true },
-    },
     sourcemap: false,
   },
   css: {
